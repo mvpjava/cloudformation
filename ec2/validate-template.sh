b461ff4626df9f89b385d4191cd70369126751b3
Andy Luis <mvpjava.email@gmail.com>
2024-11-09 15:26:28 +0000

    wrapper script to validate cfn template

---
+++ ec2/validate-template.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Check if the required number of parameters is provided
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <Cloudformation template name>"
+  exit 1
+fi
+
+CFN_TEMPLATE_NAME=$1
+
+echo "Starting $0"
+
+aws cloudformation validate-template --template-body file://$1
+
+echo "$0 Complete."
