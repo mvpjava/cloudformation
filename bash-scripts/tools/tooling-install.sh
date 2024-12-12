a3f6c744ec6cae2a720db480650e74f2b789d47b
MVP Java <mvpjava.email@gmail.com>
2024-12-12 22:02:41 -0100

    Delete bash-scripts directory

--- bash-scripts/tools/tooling-install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo yum install git -y
-sudo yum install tree -y
--- bash-scripts/vscode/vscode-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-
-sudo yum check-update
-sudo yum install -y code
