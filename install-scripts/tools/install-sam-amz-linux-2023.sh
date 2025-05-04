6c4b431887e0ae1e9e7c653799664154aee5fc08
MVP Java <mvpjava.email@gmail.com>
2025-05-04 18:08:47 +0000

    Update install-sam-amz-linux-2023.sh

--- install-scripts/tools/install-sam-amz-linux-2023.sh
+++ install-scripts/tools/install-sam-amz-linux-2023.sh
@@ -2,6 +2,11 @@
 
 #Only meant for demo to install aws sam cli on Amazon Linux 2023 AMI on x86 arch
 
+yum install git -y
+yum install tree -y
+sudo yum install python3.9-pip #  Amz Linux 2023  comes with Python 3.9 pre-installed so, get associated pip needed to
+
+# get sam cli
 curl -Lo aws-sam-cli-linux-x86_64.zip https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
 unzip aws-sam-cli-linux-x86_64.zip
 sudo ./install
